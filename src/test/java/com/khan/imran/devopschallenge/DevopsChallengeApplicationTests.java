5af69254ed21de5eaaa86101399a11a56a865f72
Imran Khan <khanmech26@gmail.com>
2022-06-11 23:33:40 +0100

    Initial commit with Spring project code.

---
+++ src/test/java/com/khan/imran/devopschallenge/DevopsChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.khan.imran.devopschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
